0a4c562a8e370911f1577dd1b57d85bae4865a9e
CATsightGK <CATsightGK@users.noreply.github.com>
2026-07-03 17:56:18 +0800

    调整制糖机JEI界面为原版配色

--- src/main/java/com/valentin4311/candycraftmod/integration/jei/SugarFactoryRecipeCategory.java
+++ src/main/java/com/valentin4311/candycraftmod/integration/jei/SugarFactoryRecipeCategory.java
@@ -59,9 +59,9 @@ public class SugarFactoryRecipeCategory implements IRecipeCategory<SugarFactoryJ
 
     @Override
     public void setRecipe(IRecipeLayoutBuilder builder, SugarFactoryJeiRecipe recipe, IFocusGroup focuses) {
-        builder.addInputSlot(8, 5)
+        builder.addInputSlot(8, 7)
             .addItemStacks(recipe.inputs());
-        builder.addOutputSlot(150, 5)
+        builder.addOutputSlot(152, 7)
             .addItemStack(recipe.output());
     }
 
